7c572978bd818b6657734c027f608dae1334498a
kikiondo <kikisvv@hotmail.com>
2018-10-27 16:22:54 -0500

    Estilos modificados del componente Registro

--- oficio-fast/src/components/App.js
+++ oficio-fast/src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import Login from './Login/index'
 import './App.css';
+
+
+import Login from './Login/index'
 import Registro from './Registro'
 
 import 'materialize-css/dist/css/materialize.min.css'
@@ -9,25 +10,8 @@ import 'materialize-css/dist/css/materialize.min.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-            <i className="material-icons right">send</i>
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Login/>
+      <div>
+        <Registro />
       </div>
     );
   }
--- oficio-fast/src/components/Registro/Form.jsx
+++ oficio-fast/src/components/Registro/Form.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import uid from 'uid'
+import './styles.css'
 
 const Formulario = ({onAddCourse}) => (
-  <form onSubmit={onAddCourse}>
-     <input type="hidden" name="id" value={uid(10)} />
 
-    <label htmlFor="Name">Nombre</label>
-    <input name="name" placeholder="Nombre" id="Name" type="text" className="validate" required/>
+  <div className="Registro-Container">
+    <h2 className="center">Registrate</h2>
+    <form onSubmit={onAddCourse}>
+      <input type="hidden" name="id" value={uid(10)} />
 
-    <label htmlFor="Email">Correo</label>
-    <input name="email" placeholder="Correo" id="Email" type="email" className="validate" required/>
+      <label htmlFor="Name">Nombre</label>
+      <input name="name" placeholder="Nombre" id="Name" type="text" className="validate" required/>
 
-    <label htmlFor="Password">Contraseña</label>
-    <input name="password" placeholder="Contraseña" id="Password" type="password" className="validate" required/>
+      <label htmlFor="Email">Correo</label>
+      <input name="email" placeholder="Correo" id="Email" type="email" className="validate" required/>
 
-    <label htmlFor="School">Escolaridad</label>
-    <input name="school" placeholder="Escolaridad" id="School" type="text" className="validate" required/>
+      <label htmlFor="Password">Contraseña</label>
+      <input name="password" placeholder="Contraseña" id="Password" type="password" className="validate" required/>
 
-    <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-      <i className="material-icons right">send</i>
-    </button>
-  </form>
+      <label htmlFor="School">Escolaridad</label>
+      <input name="school" placeholder="Escolaridad" id="School" type="text" className="validate" required/>
+
+      <button className="btn waves-effect waves-light" type="submit" name="action">Regresar</button>
+      <button className="btn waves-effect waves-light" type="submit" name="action">Registrarte</button>
+    </form>
+  </div>
 )
 
 export default Formulario
