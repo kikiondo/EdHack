c22fb43e53d265837be580cb7e6e996cfa188486
Emmanuel <jgonzalez13@ucol.mx>
2018-10-27 14:24:33 -0500

    Componente Login Terminado solo los estilos

--- oficio-fast/src/components/App.js
+++ oficio-fast/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import Login from './Login/index'
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css'
 
@@ -7,23 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-    <i class="material-icons right">send</i>
-  </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Login/>
       </div>
     );
   }
