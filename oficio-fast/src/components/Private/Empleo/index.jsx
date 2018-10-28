88f9014c55e52bc36e6469f1dbd0ab7789182bda
kikiondo <kikisvv@hotmail.com>
2018-10-28 01:46:20 -0600

    Empleo kwaii está listo

--- oficio-fast/src/components/Private/Empleo/index.jsx
+++ oficio-fast/src/components/Private/Empleo/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import './empleo.css'
+import KawaiiAnimation from "./KawaiiAnimation";
 
 class Empleo extends Component{
   constructor(){
@@ -8,7 +10,17 @@ class Empleo extends Component{
 
   render() {
     return(
-      <h1>Sección de empleo</h1>
+     <div className="Container-Empleo">
+        <h3 className="Empleo-Title">¿Qué necesitas?</h3>
+        <div className="Container-Kawaii">
+          <KawaiiAnimation />
+          <p>Busca nuevas oportunidades y explota tus talentos o ayúdanos a cambiar la vida de las personas.</p>
+        </div>
+        <div className="Container-Buttons container">
+          <button className="btn waves-effect blue darken-2">Buscar empleo</button>
+          <button className="btn waves-effect blue darken-2">Publicar empleo</button>
+        </div>
+     </div>
     )
   }
 }
