1f7b629a7625f47ca92fc1472082468c316dfd30
kikiondo <kikisvv@hotmail.com>
2018-10-28 12:58:30 -0600

    Se agregó modificaron estilos y se creó una función que agarra los datos de una api

--- oficio-fast/src/components/Private/Empleo/index.jsx
+++ oficio-fast/src/components/Private/Empleo/index.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 import './empleo.css'
 import KawaiiAnimation from "./KawaiiAnimation"
+import Information from '../Information'
 
 class Empleo extends Component{
   constructor(){
@@ -12,19 +13,20 @@ class Empleo extends Component{
 
   render() {
     return(
-      
-     <div className="Container-Empleo">
-        <h3 className="Empleo-Title">¿Qué necesitas?</h3>
-        <div className="Container-Kawaii">
-          <KawaiiAnimation />
-          <p>Busca nuevas oportunidades y explota tus talentos o ayúdanos a cambiar la vida de las personas.</p>
-        </div>
-        <div className="Container-Buttons container">
-          <Link to="/buscarempleo" className="link btn waves-effect blue darken-2">Buscar Empleo</Link>
-          <Link to="/crearempleo" className="link btn waves-effect blue darken-2">Crear Empleo</Link>
-        </div>
-     </div>
-
+    <Fragment>
+      <div className="Container-Empleo">
+          <h3 className="Empleo-Title">¿Qué necesitas?</h3>
+          <div className="Container-Kawaii">
+            <KawaiiAnimation />
+            <p>Busca nuevas oportunidades y explota tus talentos o ayúdanos a cambiar la vida de las personas.</p>
+          </div>
+          <div className="Container-Buttons container">
+            <Link to="/buscarempleo" className="link btn waves-effect blue darken-2">Buscar Empleo</Link>
+            <Link to="/crearempleo" className="link btn waves-effect blue darken-2">Crear Empleo</Link>
+          </div>
+      </div>
+      <Information />
+     </Fragment>
     )
   }
 }
