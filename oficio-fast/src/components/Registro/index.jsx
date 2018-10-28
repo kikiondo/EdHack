8b912031e7b9718bb3956e37fcac87e2414ca39d
kikiondo <kikisvv@hotmail.com>
2018-10-28 00:49:59 -0500

    Correción de errores limon

--- oficio-fast/src/components/Registro/index.jsx
+++ oficio-fast/src/components/Registro/index.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import Form from './Form'
 import { auth } from '../helpers/'
 import './styles.css'
-import logo from '../../shared/media/senorlimon.png'
 
 
 class Registro extends Component {
