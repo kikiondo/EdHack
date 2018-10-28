6174641003227ed382555dc1cf43b135f3467d93
kikiondo <kikisvv@hotmail.com>
2018-10-28 16:04:45 -0600

    Estilos mejoras de buscar empleo y se solucionaron problemas de funciones

--- oficio-fast/src/components/Private/Empleo/ShowEmpleos/index.jsx
+++ oficio-fast/src/components/Private/Empleo/ShowEmpleos/index.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import myData from '../../../../data/empleos.json';
+import { Link } from 'react-router-dom'
+import {CreditCard} from 'react-kawaii'
 import uid from 'uid'
+import './index.css'
+
 class ShowEmpleo extends Component {
   constructor()
   {
@@ -17,33 +21,38 @@ class ShowEmpleo extends Component {
   }
 
   fetchEmpleos () {
-    this.state.data.map((objeto) => {
+    this.state.data.map(objeto => {
       if(objeto.id === 343627767) {
-       this.setState({ empleos: objeto.cachedContents.top})
+       return this.setState({ empleos: objeto.cachedContents.top})
+      }
+      else {
+        return null
       }
     })
   }
 
   render() {
     return(
-     <div className="container">
-      <table className="striped">
-        <thead>
-          <tr>
-              <th>Empleos más solicitados!!!</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            this.state.empleos.map(empleo =>
-              <tr key={uid(2)}> 
-                <td>{empleo.item}</td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
-     </div>
+      <div className="container Container-BuscarEmpleo">
+        <CreditCard size={150} mood="shocked" color="#83D1FB"/>
+        <table className="striped">
+          <thead>
+            <tr>
+                <th>Empleos más solicitados!!!</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.empleos.map(empleo =>
+                <tr key={uid(2)}> 
+                  <td>{empleo.item}</td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+        <Link to="/empleos" className="Link btn waves-effect blue darken-2">Regresar</Link>
+      </div>
     )
   }
 }
