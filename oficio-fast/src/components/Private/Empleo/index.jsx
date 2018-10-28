fe777aab68352317cbcef0b7f364f118ea4811a7
JuanSantillan97 <juanyut97@hotmail.com>
2018-10-28 13:55:17 -0600

    componente navbar

--- oficio-fast/src/components/Private/Empleo/index.jsx
+++ oficio-fast/src/components/Private/Empleo/index.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import './empleo.css'
 import KawaiiAnimation from "./KawaiiAnimation"
 
+import Navbar from '../../../shared/components/Nav'
+
 class Empleo extends Component{
   constructor(){
     super()
@@ -12,19 +14,20 @@ class Empleo extends Component{
 
   render() {
     return(
-      
-     <div className="Container-Empleo">
-        <h3 className="Empleo-Title">¿Qué necesitas?</h3>
-        <div className="Container-Kawaii">
-          <KawaiiAnimation />
-          <p>Busca nuevas oportunidades y explota tus talentos o ayúdanos a cambiar la vida de las personas.</p>
+      <div className="container-papa">
+        <Navbar className="navegacion" />
+        <div className="Container-Empleo">
+          <h3 className="Empleo-Title">¿Qué necesitas?</h3>
+          <div className="Container-Kawaii">
+            <KawaiiAnimation />
+            <p>Busca nuevas oportunidades y explota tus talentos o ayúdanos a cambiar la vida de las personas.</p>
+          </div>
+          <div className="Container-Buttons container">
+            <Link to="/buscarempleo" className="link btn waves-effect blue darken-2">Buscar Empleo</Link>
+            <Link to="/crearempleo" className="link btn waves-effect blue darken-2">Crear Empleo</Link>
+          </div>
         </div>
-        <div className="Container-Buttons container">
-          <Link to="/buscarempleo" className="link btn waves-effect blue darken-2">Buscar Empleo</Link>
-          <Link to="/crearempleo" className="link btn waves-effect blue darken-2">Crear Empleo</Link>
-        </div>
-     </div>
-
+      </div>
     )
   }
 }
